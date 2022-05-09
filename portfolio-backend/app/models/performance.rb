bc584a193c2bd3afcdf9a1177cd29f5bb68e37a4
A.S. Lomoff <lomov.as@gmail.com>
2022-05-09 19:28:20 +0200

    BE: add performance-user relationship, add attachment to performance

--- portfolio-backend/app/models/performance.rb
+++ portfolio-backend/app/models/performance.rb
@@ -1,2 +1,6 @@
 class Performance < ApplicationRecord
+  default_scope -> { with_attached_avatar }
+
+  has_one_attached :recording
+  belongs_to :user
 end
--- portfolio-backend/db/migrate/20220509163259_create_performances.rb
+++ portfolio-backend/db/migrate/20220509163259_create_performances.rb
@@ -2,7 +2,7 @@ class CreatePerformances < ActiveRecord::Migration[7.0]
   def change
     create_table :performances, id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
       t.string "title", null: false, default: ""
-
+      t.references :user, type: :uuid, index: true, foreign_key: {to_table: :users}
       t.timestamps
     end
   end
